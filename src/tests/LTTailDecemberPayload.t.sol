d6ab87c1722cf8dd7d1ea29931b309ed7fe77e1c
Yonatan <john.hayun@gmail.com>
2022-12-14 15:59:50 +0200

    long tail

---
+++ src/tests/LTTailDecemberPayload.t.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.0;
+
+// testing libraries
+import "@forge-std/Test.sol";
+
+// contract dependencies
+import {GovHelpers} from "@aave-helpers/GovHelpers.sol";
+import {AaveV2Ethereum} from "@aave-address-book/AaveV2Ethereum.sol";
+import {AaveAddressBookV2} from "@aave-address-book/AaveAddressBook.sol";
+import {ProposalPayload} from "../payloads/LTTailDecemberPayload.sol";
+import {DeployMainnetProposal} from "../../script/DeployMainnetProposal.s.sol";
+import {AaveV2Helpers, ReserveConfig, ReserveTokens, InterestStrategyValues} from "./utils/AaveV2Helpers.sol";
+
+contract ProposalTailAssetsLTPayloadTest is Test {
+    address public constant AAVE_WHALE = 0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8;
+    string public constant ENS = "ENS";
+    string public constant SNX = "SNX";
+    string public constant MKR = "MKR";
+    string public constant CRV = "CRV";
+
+    uint256 public constant ENS_LTV = 4700; /// 50 -> 47
+    uint256 public constant ENS_LIQUIDATION_THRESHOLD = 5700; // 60 -> 57
+
+    uint256 public constant MKR_LTV = 6200; /// 65 -> 62
+    uint256 public constant MKR_LIQUIDATION_THRESHOLD = 6700; // 70 -> 67
+
+    uint256 public constant SNX_LTV = 4600; /// 49 -> 46
+    uint256 public constant SNX_LIQUIDATION_THRESHOLD = 6200; // 65 -> 62
+
+    uint256 public constant CRV_LTV = 5200; /// 55 -> 52
+    uint256 public constant CRV_LIQUIDATION_THRESHOLD = 5800; // 61 -> 58
+
+    uint256 public proposalId;
+    ProposalPayload public proposalPayload;
+
+    function setUp() public {
+        vm.createSelectFork(vm.rpcUrl("mainnet"), 16182710);
+
+        // Deploy Payload
+        proposalPayload = new ProposalPayload();
+
+        // Create Proposal
+        vm.prank(AAVE_WHALE);
+        proposalId = DeployMainnetProposal._deployMainnetProposal(
+            address(proposalPayload),
+            bytes32(0x5d0543d0e66abc240eceeae5ada6240d4d6402c2ccfe5ad521824dc36be71c45)
+        );
+        vm.stopPrank();
+    }
+
+    function testDAILTProposal() public {
+        require(proposalId != 0, "proposal deployed");
+        ReserveConfig[] memory allConfigsBefore = AaveV2Helpers._getReservesConfigs(
+            false,
+            AaveAddressBookV2.AaveV2Ethereum
+        );
+
+        GovHelpers.passVoteAndExecute(vm, proposalId);
+
+        ReserveConfig[] memory allConfigsAfter = AaveV2Helpers._getReservesConfigs(
+            false,
+            AaveAddressBookV2.AaveV2Ethereum
+        );
+
+        AaveV2Helpers._validateCountOfListings(0, allConfigsBefore, allConfigsAfter);
+
+        //ENS
+        ReserveConfig memory ENSconfig = AaveV2Helpers._findReserveConfig(allConfigsBefore, ENS, false);
+        ENSconfig.ltv = ENS_LTV;
+        ENSconfig.liquidationThreshold = ENS_LIQUIDATION_THRESHOLD;
+
+        AaveV2Helpers._validateReserveConfig(ENSconfig, allConfigsAfter);
+
+        //MKR
+        ReserveConfig memory MKRconfig = AaveV2Helpers._findReserveConfig(allConfigsBefore, MKR, false);
+        MKRconfig.ltv = MKR_LTV;
+        MKRconfig.liquidationThreshold = MKR_LIQUIDATION_THRESHOLD;
+
+        AaveV2Helpers._validateReserveConfig(MKRconfig, allConfigsAfter);
+
+        //SNX
+        ReserveConfig memory SNXconfig = AaveV2Helpers._findReserveConfig(allConfigsBefore, SNX, false);
+        SNXconfig.ltv = SNX_LTV;
+        SNXconfig.liquidationThreshold = SNX_LIQUIDATION_THRESHOLD;
+
+        AaveV2Helpers._validateReserveConfig(SNXconfig, allConfigsAfter);
+
+        //CRV
+        ReserveConfig memory CRVconfig = AaveV2Helpers._findReserveConfig(allConfigsBefore, CRV, false);
+        CRVconfig.ltv = CRV_LTV;
+        CRVconfig.liquidationThreshold = CRV_LIQUIDATION_THRESHOLD;
+
+        AaveV2Helpers._validateReserveConfig(CRVconfig, allConfigsAfter);
+    }
+}

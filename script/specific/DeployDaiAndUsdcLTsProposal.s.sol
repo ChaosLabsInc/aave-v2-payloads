e4d3b3439b8dc3a0b92f50df525166673cedb4ec
Yonatan <john.hayun@gmail.com>
2022-12-19 19:53:54 +0200

    fix multi deploy

--- script/specific/DeployDaiAndUsdcLTsProposal.s.sol
+++ script/specific/DeployDaiAndUsdcLTsProposal.s.sol
@@ -6,19 +6,26 @@ import {Script} from "@forge-std/Script.sol";
 import {AaveGovernanceV2, IExecutorWithTimelock} from "@aave-address-book/AaveGovernanceV2.sol";
 
 library DeployMainnetProposal {
-    function _deployMainnetProposal(address payload, bytes32 ipfsHash) internal returns (uint256 proposalId) {
-        require(payload != address(0), "ERROR: PAYLOAD can't be address(0)");
+    function _deployMainnetMultiProposals(address[] memory payloads, bytes32 ipfsHash)
+        internal
+        returns (uint256 proposalId)
+    {
         require(ipfsHash != bytes32(0), "ERROR: IPFS_HASH can't be bytes32(0)");
-        address[] memory targets = new address[](1);
-        targets[0] = payload;
-        uint256[] memory values = new uint256[](1);
-        values[0] = 0;
-        string[] memory signatures = new string[](1);
-        signatures[0] = "execute()";
-        bytes[] memory calldatas = new bytes[](1);
-        calldatas[0] = "";
-        bool[] memory withDelegatecalls = new bool[](1);
-        withDelegatecalls[0] = true;
+
+        address[] memory targets = new address[](payloads.length);
+        uint256[] memory values = new uint256[](payloads.length);
+        string[] memory signatures = new string[](payloads.length);
+        bytes[] memory calldatas = new bytes[](payloads.length);
+        bool[] memory withDelegatecalls = new bool[](payloads.length);
+
+        for (uint256 i = 0; i < payloads.length; i++) {
+            require(payloads[i] != address(0), "ERROR: PAYLOAD can't be address(0)");
+            targets[i] = payloads[i];
+            values[i] = 0;
+            signatures[i] = "execute()";
+            calldatas[i] = "";
+            withDelegatecalls[i] = true;
+        }
 
         return
             AaveGovernanceV2.GOV.create(
@@ -36,16 +43,15 @@ library DeployMainnetProposal {
 contract DeployProposal is Script {
     function run() external {
         vm.startBroadcast();
-        //USDC LT:
-        DeployMainnetProposal._deployMainnetProposal(
-            address(0xeca5bdf0C2b352cBE2D9A19b555E1EC269d4765C), //USDC payload
+
+        address[] memory payloads = new address[](2);
+        payloads[0] = 0xeca5bdf0C2b352cBE2D9A19b555E1EC269d4765C; //USDC
+        payloads[1] = 0x60bCd1CaF97c3fCbC35Bf92A8852728420C34FB5; //DAI
+        DeployMainnetProposal._deployMainnetMultiProposals(
+            payloads,
             bytes32(0x5d0543d0e66abc240eceeae5ada6240d4d6402c2ccfe5ad521824dc36be71c45) // TODO: replace with actual ipfshash
         );
-        //DAI LT:
-        DeployMainnetProposal._deployMainnetProposal(
-            address(0x60bCd1CaF97c3fCbC35Bf92A8852728420C34FB5), //DAI payload
-            bytes32(0x5d0543d0e66abc240eceeae5ada6240d4d6402c2ccfe5ad521824dc36be71c45) // TODO: replace with actual ipfshash
-        );
+
         vm.stopBroadcast();
     }
 }

7e71c52d05856a6079cf8a9f5ed402dfe76a236f
altmark <michala@chaoslabs.xyz>
2023-02-28 10:35:55 +0200

    work

--- src/payloads/MKRPayload-Feb26.sol
+++ src/payloads/MKRPayload-Feb26.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
-import {AaveV2Ethereum} from "@aave-address-book/AaveV2Ethereum.sol";
+import {AaveV2Ethereum, AaveV2EthereumAssets } from "@aave-address-book/AaveV2Ethereum.sol";
 
 /**
  * @title Chaos <> AAVE Proposal
@@ -11,7 +11,7 @@ import {AaveV2Ethereum} from "@aave-address-book/AaveV2Ethereum.sol";
  * Snapshot: https://snapshot.org/#/aave.eth/proposal/0x9f2c87a0eb6296ca1ef45bc3ce70cbd7884a0c7b960f17f32a0742abf89c2b8a
  */
 contract ProposalPayload {
-    address public constant MKR = 0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2;
+    address public constant MKR = AaveV2EthereumAssets.MKR_UNDERLYING;
     uint256 public constant MKR_LTV = 5900; ///  59
     uint256 public constant MKR_LIQUIDATION_THRESHOLD = 6400; // 64
     uint256 public constant MKR_LIQUIDATION_BONUS = 10750; //unchanged
